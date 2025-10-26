0fe73102245cce02bf7700ccc8872a978bea1d3c
NCR-2005 <nikhilcrpeddinti@gmail.com>
2025-10-26 09:42:26 +0530

    created Appointment in AppointmentService

--- src/main/java/com/flmhospitals/model/Appointment.java
+++ src/main/java/com/flmhospitals/model/Appointment.java
@@ -3,7 +3,6 @@ package com.flmhospitals.model;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;

84e89c31de5c877e82ca5fff7c4bfe3ba473e91b
PrasanthPulapa^c <pulapaprasanth@gmail.com>
2025-10-29 13:02:16 +0530

    Added AppointmentDTOBuilder

---
+++ src/main/java/com/flmhospitals/builder/AppointmentDTOBuilder.java
@@ -0,0 +1,20 @@
+package com.flmhospitals.builder;
+
+import com.flmhospitals.dto.AppointmentResponseDTO;
+import com.flmhospitals.model.Appointment;
+
+public class AppointmentDTOBuilder {
+
+	 public static AppointmentResponseDTO buildAppointmentResponseDTO(Appointment appointment, String patientName, String doctorName) {
+	        return AppointmentResponseDTO.builder()
+	                .appointmentId(appointment.getAppointmentId())
+	                .patientName(patientName)
+	                .doctorName(doctorName)
+	                .appointmentDate(appointment.getAppointmentDate())
+	                .startTime(appointment.getStartTime())
+	                .endTime(appointment.getEndTime())
+	                .status(appointment.getStatus())
+	                .notes(appointment.getNotes())
+	                .build();
+	    }
+}

ec9c429dbc8d6e45ac7acda1f13f8c26b2108948
PrasanthPulapa^c <pulapaprasanth@gmail.com>
2025-10-29 21:40:00 +0530

    Removed that lines

--- src/main/java/com/flmhospitals/builder/AppointmentDTOBuilder.java
+++ src/main/java/com/flmhospitals/builder/AppointmentDTOBuilder.java
@@ -1,6 +1,7 @@
 package com.flmhospitals.builder;
 
 import com.flmhospitals.dto.AppointmentResponseDTO;
+
 import com.flmhospitals.model.Appointment;
 
 public class AppointmentDTOBuilder {
@@ -8,8 +9,6 @@ public class AppointmentDTOBuilder {
 	 public static AppointmentResponseDTO buildAppointmentResponseDTO(Appointment appointment, String patientName, String doctorName) {
 	        return AppointmentResponseDTO.builder()
 	                .appointmentId(appointment.getAppointmentId())
-	                .patientName(patientName)
-	                .doctorName(doctorName)
 	                .appointmentDate(appointment.getAppointmentDate())
 	                .startTime(appointment.getStartTime())
 	                .endTime(appointment.getEndTime())

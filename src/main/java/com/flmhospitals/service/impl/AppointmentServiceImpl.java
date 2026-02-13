f8008508bd3c89f2b79f096dae921d2d030f3829
Jagadeesh378 <jagadeeshkemburu2000@gmail.com>
2026-02-13 11:05:10 +0530

    resolved merge conflicts

--- src/main/java/com/flmhospitals/service/impl/AppointmentServiceImpl.java
+++ src/main/java/com/flmhospitals/service/impl/AppointmentServiceImpl.java
@@ -2,7 +2,9 @@ package com.flmhospitals.service.impl;
 
 import java.time.LocalDate;
 import java.util.List;
+
 import org.springframework.stereotype.Service;
+
 import com.flmhospitals.builder.AppointmentBuilder;
 import com.flmhospitals.builder.AppointmentDTOBuilder;
 import com.flmhospitals.clients.DoctorClient;
@@ -16,6 +18,7 @@ import com.flmhospitals.exception.DoctorUnAvailableException;
 import com.flmhospitals.exception.InvalidTimeException;
 import com.flmhospitals.model.Appointment;
 import com.flmhospitals.service.AppointmentService;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Service
@@ -134,6 +137,25 @@ public class AppointmentServiceImpl implements AppointmentService {
 		appointment.setStatus("CANCELLED");
 		appointmentRepository.save(appointment);
 		return true;
+	}
+	
+	public AppointmentResponseDTO getAppointmentDetails(String appointmentId) {
+		
+		Appointment appointment = appointmentRepository.findById(appointmentId)
+		.orElseThrow(()->new AppointmentNotFoundException("no appointment found with the the Id :" + appointmentId));
+		
+		AppointmentResponseDTO appointmentResponseDTO = AppointmentDTOBuilder.buildAppointmentResponseDTO(appointment);
+		
+		String doctorName = doctorClient.getDoctorName(appointment.getDoctorId());
+		
+		String PatientName = patientClient.getPatientName(appointment.getPatientId());
+		
+		appointmentResponseDTO.setDoctorName(doctorName);
+		
+		appointmentResponseDTO.setPatientName(PatientName);
+		
+		 return appointmentResponseDTO;
+
 	}
 
 }

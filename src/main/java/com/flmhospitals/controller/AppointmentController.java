b8a2ee7fa312b52d177e72dd54e00d49fcb8f91a
harsha <harshareddy2038@gmail.com>
2026-02-17 10:14:59 +0530

    Refactored appointment controller

--- src/main/java/com/flmhospitals/controller/AppointmentController.java
+++ src/main/java/com/flmhospitals/controller/AppointmentController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import com.flmhospitals.dto.RescheduleAppointmentDTO;
 import com.flmhospitals.model.Appointment;
 import com.flmhospitals.service.AppointmentService;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/appointments")
 public class AppointmentController {
